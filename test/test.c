49a805dea39ea97970422bae1f4e49a0802ca3b1
Stephen Mathieson <smath23@gmail.com>
2014-03-05 09:23:00 -0500

    Free results in tests

--- test/test.c
+++ test/test.c
@@ -8,18 +8,21 @@
 
 describe("str-replace", {
   it("should replace single matches", {
-    assert_str_equal("goodbye world",
-      str_replace("hello world", "hello", "goodbye"));
+    char *replaced = str_replace("hello world", "hello", "goodbye");
+    assert_str_equal("goodbye world", replaced);
+    free(replaced);
   });
 
   it("should replace multiple matches", {
-  char small[] = "She lived in a small house "
-                 "with a small garage on the "
-                 "outskirts of a small city.";
-  char big[] = "She lived in a big house with "
-               "a big garage on the outskirts "
-               "of a big city.";
-    assert_str_equal(small, str_replace(big, "big", "small"));
+    char small[] = "She lived in a small house "
+                   "with a small garage on the "
+                   "outskirts of a small city.";
+    char big[] = "She lived in a big house with "
+                 "a big garage on the outskirts "
+                 "of a big city.";
+    char *replaced = str_replace(big, "big", "small");
+    assert_str_equal(small, replaced);
+    free(replaced);
   });
 
   it("should return a copy of the string when provided no matches", {
@@ -28,5 +31,6 @@ describe("str-replace", {
     assert_str_equal(str, replaced);
     str[1] = 'z';
     assert_str_not_equal(str, replaced);
+    free(replaced);
   });
 });
