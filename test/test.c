279dc6c2bd7c7368a5eda3d438dfa47ce7212adc
Leonardo Esparis <leoxnidas.c.14@gmail.com>
2015-12-29 09:59:41 -0430

    new test added

--- test/test.c
+++ test/test.c
@@ -1,36 +1,14 @@
-
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <assert.h>
 #include "str-replace.h"
-#include "describe/describe.h"
 
-describe("str-replace", {
-  it("should replace single matches", {
-    char *replaced = str_replace("hello world", "hello", "goodbye");
-    assert_str_equal("goodbye world", replaced);
-    free(replaced);
-  });
 
-  it("should replace multiple matches", {
-    char small[] = "She lived in a small house "
-                   "with a small garage on the "
-                   "outskirts of a small city.";
-    char big[] = "She lived in a big house with "
-                 "a big garage on the outskirts "
-                 "of a big city.";
-    char *replaced = str_replace(big, "big", "small");
-    assert_str_equal(small, replaced);
-    free(replaced);
-  });
+int
+main(void)
+{
 
-  it("should return a copy of the string when provided no matches", {
-    char str[] = "hello world";
-    char *replaced = str_replace(str, "foo", "bar");
-    assert_str_equal(str, replaced);
-    str[1] = 'z';
-    assert_str_not_equal(str, replaced);
-    free(replaced);
-  });
-});
+	char phrase[] = "linux OS it is better for pentesters";
+	printf("normal: %s\n", phrase);
+	printf("replaced: %s\n", str_replace(phrase, "linux", "Window"));
+
+	return 0;
+}
